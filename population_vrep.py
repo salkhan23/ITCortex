51a19e412a16b59ecb67b2909d0efd88acd3a05f
Salman <salkhan23@gmail.com>
2015-08-06 16:59:56 -0400

    For a single object, (1) get projection on the vision senors projection plane, (2) figure out wether object lies within the projection frame of the vision sensor, (3) extract ground truth, x & y position and size from the distance to the object all in degrees

--- population_vrep.py
+++ population_vrep.py
@@ -43,7 +43,7 @@ class VrepObject:
     def __init__(self, name, handle, max_dimension):
         self.name = name
         self.handle = handle
-        self.size = max_dimension
+        self.max_dimension = max_dimension
 
 
 def connect_vrep(sim_stop_time_s):
@@ -159,7 +159,7 @@ def print_objects(objects):
 
     for obj in objects:
         print("\t%s: handle=%d, max_dimension=%0.1f"
-              % (obj.name.ljust(longest_name), obj.handle, obj.size))
+              % (obj.name.ljust(longest_name), obj.handle, obj.max_dimension))
 
 
 def set_robot_velocity(c_id, target_velocity):
@@ -208,7 +208,7 @@ def get_vision_sensor_parameters(c_id):
         n_z      : Near clipping plane of vision sensor
         f_z       : Far clipping plane of vision sensor
     """
-    res, vs_handle = vrep.simxGetObjectHandle(
+    res, vis_sen_handle = vrep.simxGetObjectHandle(
         c_id,
         'it_cortex_robot_vision_sensor',
         vrep.simx_opmode_oneshot_wait)
@@ -218,7 +218,7 @@ def get_vision_sensor_parameters(c_id):
 
     res, angle = vrep.simxGetObjectFloatParameter(
         c_id,
-        vs_handle,
+        vis_sen_handle,
         VS_PERSPECTIVE_PROJECTION_ANGLE,
         vrep.simx_opmode_oneshot_wait)
     if res != vrep.simx_return_ok:
@@ -226,7 +226,7 @@ def get_vision_sensor_parameters(c_id):
 
     res, resolution_x = vrep.simxGetObjectIntParameter(
         c_id,
-        vs_handle,
+        vis_sen_handle,
         VS_RESOLUTION_X,
         vrep.simx_opmode_oneshot_wait)
     if res != vrep.simx_return_ok:
@@ -234,7 +234,7 @@ def get_vision_sensor_parameters(c_id):
 
     res, resolution_y = vrep.simxGetObjectIntParameter(
         c_id,
-        vs_handle,
+        vis_sen_handle,
         VS_RESOLUTION_Y,
         vrep.simx_opmode_oneshot_wait)
     if res != vrep.simx_return_ok:
@@ -244,7 +244,7 @@ def get_vision_sensor_parameters(c_id):
 
     res, z_n = vrep.simxGetObjectFloatParameter(
         c_id,
-        vs_handle,
+        vis_sen_handle,
         VS_NEAR_CLIPPING_PLANE,
         vrep.simx_opmode_oneshot_wait)
     if res != vrep.simx_return_ok:
@@ -252,18 +252,52 @@ def get_vision_sensor_parameters(c_id):
 
     res, z_f = vrep.simxGetObjectFloatParameter(
         c_id,
-        vs_handle,
+        vis_sen_handle,
         VS_FAR_CLIPPING_PLANE,
         vrep.simx_opmode_oneshot_wait)
     if res != vrep.simx_return_ok:
         raise Exception("Failed to get VS_FAR_CLIPPING_PLANE. Error code %d" % res)
 
-    return angle, ar, z_n, z_f
+    return angle, ar, z_n, z_f, vis_sen_handle
+
+
+def get_object_position(c_id, object_handle, reference_frame_handle):
+    """
+     Get position (x,y,z) coordinates of object specified by object_handle with respect to
+     the reference frame of object specified by reference_frame_handle.
+    :param c_id                     : connected scene id
+    :param object_handle            : vrep handle of object
+    :param reference_frame_handle   : vrep handle of objects whose reference frame to use
+    :return: (x,y,z) co-ordinates of target object
+    """
+    res, position = vrep.simxGetObjectPosition(
+        c_id,
+        object_handle,
+        reference_frame_handle,
+        vrep.simx_opmode_buffer)
+
+    if res != vrep.simx_return_ok:
+        print('Initializing position acquisition function')
+        res, position = vrep.simxGetObjectPosition(
+            c_id,
+            object_handle,
+            reference_frame_handle,
+            vrep.simx_opmode_streaming)
+
+        time.sleep(0.1)  # wait 100ms after initializing
+        res, position = vrep.simxGetObjectPosition(
+            c_id,
+            object_handle,
+            reference_frame_handle,
+            vrep.simx_opmode_buffer)
+
+    return position
 
 
 def main():
 
     t_stop = 20  # Simulation stop time in seconds
+    t_start = time.time()
     client_id = connect_vrep(t_stop)
 
     try:
@@ -275,14 +309,97 @@ def main():
         print_objects(objects_array)
 
         # Get IT Cortex Robot Vision sensor parameters
-        alpha_rad, aspect_ratio, z_near, z_far = get_vision_sensor_parameters(client_id)
+        alpha_rad, aspect_ratio, z_near, z_far, vs_handle = get_vision_sensor_parameters(client_id)
 
         # Construct vision sensor projection matrix
+        # This Projection Matrix, scales x, y, z axis to range (-1, 1) to make it easier to detect
+        # whether the object falls within the projection frame of the vision sensor.
+        # Ref: http://ogldev.atspace.co.uk/www/tutorial12/tutorial12.html
+        a = 1.0/(aspect_ratio*np.tan(alpha_rad/2.0))
+        b = 1.0/np.tan(alpha_rad/2.0)
+        c = -(z_near + z_far) / (z_near - z_far)
+        d = (2*z_near*z_far) / (z_near - z_far)
+
+        p_mat = np.array([[a, 0, 0, 0],
+                          [0, b, 0, 0],
+                          [0, 0, c, d],
+                          [0, 0, 1, 0]])
+
         # Generate IT Population ----------------------------------------------------------------
 
         # Start IT Cortex Robot -----------------------------------------------------------------
         set_robot_velocity(client_id, 0.2)
-        time.sleep(15)
+        time.sleep(0.1)
+
+        # start with a single object
+        obj_idx = None
+        for idx, vrep_obj in enumerate(objects_array):
+            if vrep_obj.name == 'Cuboid':
+                obj_idx = idx
+        print obj_idx
+
+        # Get projection of all objects in the vision senor projection plane
+        if obj_idx is not None:
+            while time.time() < (t_start + t_stop):
+                pos_world = get_object_position(
+                    client_id,
+                    objects_array[obj_idx].handle,
+                    vs_handle)
+
+                # Convert to homogeneous coordinates
+                pos_world.append(1)
+                pos_world = np.array(pos_world)
+
+                # Part 1. Project to Video Sensor Plane
+                camera_homogeneous = np.dot(p_mat, pos_world)
+
+                # Part 2. Divide by Chw (actual z coordinate)
+                e = 1.0/camera_homogeneous[-1]
+                p_mat2 = np.array([[e, 0, 0, 0],
+                                   [0, e, 0, 0],
+                                   [0, 0, e, 0],
+                                   [0, 0, 0, 1]])
+
+                camera_cartesian = np.dot(p_mat2, camera_homogeneous)
+                # print camera_cartesian
+
+                # Check if object list within projection frame
+                epsilon = 1*10**-3
+
+                if ((-1 - epsilon <= camera_cartesian[0] <= 1 + epsilon) and
+                    (-1 - epsilon <= camera_cartesian[1] <= 1 + epsilon) and
+                    (-1 - epsilon <= camera_cartesian[2] <= 1 + epsilon)):
+                    in_projection_frame = True
+
+                    # x,y coordinates in degrees
+                    # --------------------------
+                    # (1) Normalize calculated coordinates so they have the same scale in the x & y
+                    #     direction (by multiply x by the aspect_ratio)
+                    # (2) Convert to degrees. Assume visual span covers the range of x. -ar to ar
+                    #     Size in degrees
+                    # ---------------
+                    # (3) np.pi * max_dimension / (2 * aspect_ratio * z * tan(alpha_rad/2)).
+                    #     See Notes.
+                    f = np.pi / 2
+                    g = np.pi / (2 * aspect_ratio)
+                    h = np.pi* objects_array[obj_idx].max_dimension / \
+                        (2 * aspect_ratio * camera_cartesian[-1] * np.tan(alpha_rad/2))
+
+                    ground_truth_mat = np.array([[f, 0, 0, 0],
+                                                 [0, g, 0, 0],
+                                                 [0, 0, h, 0],
+                                                 [0, 0, 0, 0]])
+
+                    ground_truth = np.dot(ground_truth_mat, camera_cartesian)
+
+                    print (objects_array[obj_idx].name, ground_truth[0], ground_truth[1], ground_truth[2])
+
+
+
+
+
+
+
 
     finally:
         # Stop Simulation -------------------------------------------------------------_---------

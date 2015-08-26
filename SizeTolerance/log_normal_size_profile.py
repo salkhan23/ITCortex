c361621a1e427e4431471e733add3a0d6655b52c
Salman <salkhan23@gmail.com>
2015-08-26 15:55:14 -0400

    Converet Log normal size tolerence profile to use radian. 2 Some Code cleanup for log_normal_size_profile

--- SizeTolerance/log_normal_size_profile.py
+++ SizeTolerance/log_normal_size_profile.py
@@ -4,41 +4,39 @@ import scipy.stats as ss
 
 
 class LogNormalSizeProfile:
-    def __init__(self, pos_tol_deg):
+    def __init__(self, pol_tol):
         """
         Generate a lognormal size tuning profile for a neuron. Reference Ito-95.
 
         REQUIRED PARAMETERS:
-        :param pos_tol_deg: 2x the standard deviation of the RF spatial extent of the neuron
-                            in degrees. It comes from the position profile. Specifically from
-                            gaussianPositionProfile.
+        :param pol_tol: 2x the standard deviation of the RF spatial extent of the neuron
+                        in radians of eccentricity. It comes from the position profile.
         """
-        self.params = {}
         self.type = 'lognormal'
 
         # TODO: Check required parameters
-        # pos_tol_deg is defined as 2x the standard deviation of the RF spatial extent of the
+        # pol_tol is defined as 2x the standard deviation of the RF spatial extent of the
         # neuron. it comes from the position profile. Specifically from gaussianPositionProfile
 
-        # Convert from pos_tol_deg to rfSize as defined in Ito 95 paper: Square root of the area
+        # Convert from pol_tol to rf_size as defined in Ito 95 paper: Square root of the area
         # extent of the neuron.
-        #   pos_tol_deg = 2 * r
-        #   rf_size     = np.sqrt(pi) * r = pos_tol_deg / 2 * np.sqrt(pi)
-        self.params['rf_size'] = pos_tol_deg / 2 * np.sqrt(np.pi)
+        #   pol_tol = 2 * r
+        #   rf_size     = np.sqrt(pi) * r = pol_tol / 2 * np.sqrt(pi)
+        self.rf_size = pol_tol / 2 * np.sqrt(np.pi)
 
         # Given a receptive field size, get max. stimulus size supported.
         #   rf_size       = area extent of RF, if assume circular = np.sqrt(np.pi) * r
         #   stim_size     = distance between the the outer edges along the longest axis
         #                   of the stimulus. If we assume a circular stimulus = 2r
-        self.params['max_stim_size'] = self.params['rf_size'] / np.sqrt(np.pi) * 2
+        self.max_stim_size = np.sqrt(self.rf_size / np.sqrt(np.pi)) * 2
 
         # Minimum stimulus size = 0.08 degrees, from Ito-95. Below this size, the object is
-        # too small to allow proper recognition.
-        self.params['min_stim_size'] = 0.08
+        # too small to allow proper recognition. Converted to radians.
+        self.min_stim_size = 0.08 * np.pi / 180.0
 
         # Get parameters for the Lognormal distribution.
-        self.params['pref_size'] = self.__get_preferred_size()
-        self.params['size_bw'] = self.__get_size_bandwidth()
+        self.pref_size = self.__get_preferred_size()
+        self.size_bw = self.__get_size_bandwidth()
 
     def __get_preferred_size(self):
         """
@@ -47,10 +45,10 @@ class LogNormalSizeProfile:
         and min value of min_stimulus size.
 
         Best fit for the preferred size distribution presented in the paper was found to be a
-        lognormal distribution with shape=0.80 and scale = 5.40 and loc = 0. For details see
-        logNormalFit.py.
+        lognormal distribution with shape=0.80 and scale = 5.40 and loc = 0 (In degrees).
+        For details see logNormalFit.py
 
-        :rtype : preferred stimulus size in degrees.
+        :rtype : preferred stimulus size in radians of eccentricity.
 
         # THE LOG NORMAL FIT:
         Maximum stimulus size tested was 31x29 degrees. The distribution of optimum size (Fig. 6)
@@ -61,14 +59,16 @@ class LogNormalSizeProfile:
 
         We assume a peak at 3.4 and a long tail ending at the maximum stimulus size.
         """
-        preferred_size = ss.lognorm.rvs(s=0.80, scale=5.40, size=1)
+        preferred_size = ss.lognorm.rvs(s=0.80, scale=5.40, size=1) * np.pi / 180.0
+        print preferred_size
 
-        preferred_size = np.max((preferred_size, self.params['min_stim_size']))
-        preferred_size = np.min((preferred_size, self.params['max_stim_size']))
+        preferred_size = np.max((preferred_size, self.min_stim_size))
+        preferred_size = np.min((preferred_size, self.max_stim_size))
 
         return preferred_size
 
-    def __get_size_bandwidth(self):
+    @staticmethod
+    def __get_size_bandwidth():
         """
         Generate a size tolerance bandwidth for the neuron based on the distribution given in
         figure 2+7 of Ito 95.
@@ -85,61 +85,59 @@ class LogNormalSizeProfile:
         """
         return ss.lognorm.rvs(s=0.30, scale=1.90, loc=0, size=1)
 
-    def firing_rate_modifier(self, stimulus_size, deg2pixel=1):
+    def firing_rate_modifier(self, stimulus_size):
         """
         Returns the normalized firing rate of the neuron to stimulus of given size.
 
-        :param stimulus_size: The distance in pixels between the outer edges along the longest
-                              axis of the stimulus
-        :param deg2pixel    : Degree to pixel value conversion factor
-        :return             : Normalized firing rate
+        :param stimulus_size: The distance in Radians between the outer edges along the longest
+                              axis of the stimulus.
+        :rtype             : Normalized firing rate
         """
-        log_mu = np.log2(self.params["pref_size"])
-        log_sigma = (self.params["size_bw"] / 2) / np.sqrt(2 * np.log(2))
+        log_mu = np.log2(self.pref_size)
+        log_sigma = (self.size_bw / 2) / np.sqrt(2 * np.log(2))
 
-        # Convert to stimulus size to degrees from pixels
-        stimulus_size_deg = stimulus_size / float(deg2pixel)
+        fire_rate = np.exp(-(np.log2(stimulus_size) - log_mu) ** 2 / (2 * log_sigma ** 2))
 
-        # Neuron does not respond to stimuli outsize its maximum supported size. This allows
+        # Neuron does not respond to stimuli outside its maximum supported size. This allows
         # the different type of tuning profiles as described in the paper.(1) <2 octaves log normal
         # (2) >5 octaves bandwidth and (3) responds only to the largest stimulus size.
-        mask_upper = stimulus_size_deg <= self.params['max_stim_size']
-        mask_lower = stimulus_size_deg > self.params['min_stim_size']
-
-        fire_rate = np.exp(-(np.log2(stimulus_size_deg) - log_mu) ** 2 / (2 * log_sigma ** 2))
+        mask_upper = stimulus_size <= self.max_stim_size
+        mask_lower = stimulus_size > self.min_stim_size
 
         return fire_rate * mask_upper * mask_lower
 
     def print_parameters(self):
-        print("Profile: %s" % self.type)
-        print("Valid stimulus size range: (%0.2f, %0.2f) degrees"
-              % (self.params['min_stim_size'], self.params['max_stim_size']))
-        print("Preferred Stimulus Size: %0.2f degrees" % self.params['min_stim_size'])
-        print("Size bandwidth: %0.2f octaves" % self.params['size_bw'])
-        print ("RF Size (area extent of receptive field): %0.2f" % self.params['rf_size'])
+        print("Profile                      = %s" % self.type)
+        print("Valid stimulus size range    = [%0.4f, %0.4f] (Radians)"
+              % (self.min_stim_size, self.max_stim_size))
+        print("Preferred Stimulus Size      = %0.4f (Radians)" % self.pref_size)
+        print("Size bandwidth               = %0.4f octaves" % self.size_bw)
+        print ("RF Size (area extent of RF) = %0.4f" % self.rf_size)
 
     def plot_size_tolerance(self, axis=None):
-        x = np.linspace(0, self.params['max_stim_size']*1.2, num=100)
+        x = np.linspace(0, self.max_stim_size*1.2, num=100)
 
         if axis is None:
             f, axis = plt.subplots()
 
         axis.plot(x, self.firing_rate_modifier(x))
-        axis.set_xlabel('Stimulus Size (Degrees)')
+        axis.set_xlabel('Stimulus Size (Radians)')
         axis.set_ylabel('Normalized Firing Rate')
-        axis.set_title("Size Tolerance (Degrees): bandwidth %0.2f, preferred size %0.2f"
-                       % (self.params['size_bw'], self.params['pref_size']))
+        axis.set_title("Size Tolerance: bandwidth %0.2f octaves, preferred size %0.2f Radians"
+                       % (self.size_bw, self.pref_size))
         axis.grid()
 
 if __name__ == "__main__":
     plt.ion()
-    n1 = LogNormalSizeProfile(pos_tol_deg=15)
+    n1 = LogNormalSizeProfile(pol_tol=15/180.0*np.pi)
     n1.print_parameters()
 
     n1.plot_size_tolerance()
 
-    stimSize = 3
+    stimSize = 3 / 180.0 * np.pi
     print ("N1 Firing Rate to stimulus of size %0.2f=%0.2f"
-           % (stimSize, n1.firing_rate_modifier(3)))
+           % (stimSize, n1.firing_rate_modifier(stimSize)))
 
-    # TODO: Generate a population of neurons and display their size tolerance
+    stimSize = n1.pref_size
+    print ("N1 Firing Rate to stimulus of size %0.2f=%0.2f"
+           % (stimSize, n1.firing_rate_modifier(stimSize)))

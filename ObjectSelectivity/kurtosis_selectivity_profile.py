Fix getting object rate modifer calculation to not use the lower case of the object name and use the case sensitive object name. Also make this consistent with power_law_selectivity_profile

ObjectSelectivity/kurtosis_selectivity_profile.py
@@ -41,7 +41,7 @@ class KurtosisSparseness:
         self.a = self.__get_distribution_shape_parameter()
         self.b = self.__get_distribution_scale_parameter()
 
-        self.objects = {item: self.__get_object_preference(np.random.uniform(size=1))
+        self.objects = {item: self.__get_object_preference(np.float(np.random.uniform(size=1)))
                         for item in list_of_objects}
 
         self.activity_fraction_measured = \

it_neuron_vrep.py
@@ -158,7 +158,7 @@ class Neuron:
         y_arr = np.array(y_arr)
         size_arr = np.array(size_arr)
 
-        obj_pref_list = np.array([self.selectivity.objects.get(obj.lower(), 0) for obj in objects])
+        obj_pref_list = np.array([self.selectivity.objects.get(obj, 0) for obj in objects])
 
         # Get position rate modifiers they will by used to weight isolated responses to get a
         # single clutter response.
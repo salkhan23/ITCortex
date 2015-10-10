74ddbf5e097908bd8a0eac5f0029b2968a020899
Salman <salkhan23@gmail.com>
2015-10-10 18:40:13 -0400

    Add a function to plot the selectivity (activity fraction) vs the position tolerance distribution

--- population_utils.py
+++ population_utils.py
@@ -53,3 +53,33 @@ def plot_population_obj_preferences(it_population, axis=None):
     axis.set_ylabel('Normalized Firing Rate')
     axis.set_xlabel('Ranked Object Preferences')
     axis.set_title('Population Object Preferences')
+
+
+def plot_selectivity_vs_position_tolerance(it_population, axis=None):
+    """  Plot population selectivity, activity fraction, verses  position tolerance """
+
+    if axis is None:
+        f, axis = plt.subplots()
+
+    position_tolerance = [neuron.position.position_tolerance for neuron in it_population]
+    position_tolerance = np.array(position_tolerance)
+
+    selectivity = [neuron.selectivity.activity_fraction_measured for neuron in it_population]
+    selectivity = np.array(selectivity)
+
+    axis.scatter(selectivity, position_tolerance, marker='+', s=60, color='green',
+                 label='Generated Data')
+
+    # Plot the linear regression fit as well.
+    m, b = np.polyfit(selectivity, position_tolerance, 1)
+
+    plt.plot(selectivity, m*selectivity + b, ':', color='green')
+
+    f_size = 34
+    axis.set_ylabel('Position Tolerance (Radians)', fontsize=f_size)
+    axis.set_xlabel('Selectivity (Activity Fraction)', fontsize=f_size)
+    axis.set_title('Position Tolerance vs Selectivity',fontsize=f_size+10)
+    axis.tick_params(axis='x', labelsize=f_size)
+    axis.tick_params(axis='y', labelsize=f_size)
+
+    plt.legend(loc='best', fontsize=f_size)

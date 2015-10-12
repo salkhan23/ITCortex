6e2a71658b6a07b56dc01bfa51e4b53875f2cbc6
Salman <salkhan23@gmail.com>
2015-10-11 23:49:11 -0400

    Add plots for single neuron selectivity and population sparseness

--- population_utils.py
+++ population_utils.py
@@ -9,6 +9,16 @@ Created on Mon Aug  22 17:30:01 2015
 ----------------------------------------------------------------------------------------------"""
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+import sys
+
+# Do relative import of the main folder to get files in sibling directories
+top_level_dir_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+if top_level_dir_path not in sys.path:
+    sys.path.append(top_level_dir_path)
+
+from ObjectSelectivity.kurtosis_selectivity_profile import calculate_kurtosis
 
 
 def population_max_firing_rate(it_population):
@@ -24,7 +34,7 @@ def population_max_firing_rate(it_population):
 
 
 def plot_population_selectivity_distribution(it_population, axis=None):
-    """Plot selectivity distribution of the population """
+    """Plot selectivity distribution (activity fractions) of the population """
     if axis is None:
         f, axis = plt.subplots()
 
@@ -37,6 +47,89 @@ def plot_population_selectivity_distribution(it_population, axis=None):
     axis.set_xlabel('Selectivity (Activity Fraction)')
     axis.set_title('Population Selectivity Distribution')
     axis.legend()
+
+
+def plot_single_neuron_selectivities(it_population, axis=None):
+    """ Plot histogram of single neuron selectivity (kurtosis) across population """
+
+    if axis is None:
+        f, axis = plt.subplots()
+
+    objects = it_population[0].selectivity.objects.keys()
+
+    single_neuron_selectivity = []
+    for neuron in it_population:
+
+        firing_rates = []
+
+        for obj in objects:
+            firing_rates.append(neuron.selectivity.objects[obj] * neuron.max_fire_rate)
+
+        single_neuron_selectivity.append(calculate_kurtosis(np.array(firing_rates)))
+
+    axis.hist(single_neuron_selectivity, bins=np.arange(0, 100, step=1))
+
+    font_size = 34
+    axis.set_ylabel('Frequency', fontsize=font_size)
+    # axis.set_xlabel('Kurtosis', fontsize=font_size)
+    axis.tick_params(axis='x', labelsize=font_size)
+    axis.tick_params(axis='y', labelsize=font_size)
+
+    axis.annotate('Mean=%0.2f' % np.mean(single_neuron_selectivity),
+                  xy=(0.95, 0.9),
+                  xycoords='axes fraction',
+                  fontsize=font_size,
+                  horizontalalignment='right',
+                  verticalalignment='top')
+
+    axis.annotate('N=%d' % len(single_neuron_selectivity),
+                  xy=(0.95, 0.82),
+                  xycoords='axes fraction',
+                  fontsize=font_size,
+                  horizontalalignment='right',
+                  verticalalignment='top')
+
+    axis.set_title('Single Neuron Selectivity', fontsize=font_size)
+
+
+def plot_population_sparseness(it_population, axis=None):
+    """ Plot histogram of population sparseness (kurtosis) of each object in the population """
+
+    objects = it_population[0].selectivity.objects.keys()
+
+    population_sparseness = []
+
+    for obj in objects:
+
+        firing_rates = []
+        for neuron in it_population:
+            firing_rates.append(neuron.selectivity.objects[obj] * neuron.max_fire_rate)
+
+        population_sparseness.append(calculate_kurtosis(np.array(firing_rates)))
+
+    axis.hist(population_sparseness, bins=np.arange(0, 100, step=1))
+
+    font_size = 34
+    axis.set_ylabel('Frequency', fontsize=font_size)
+    axis.set_xlabel('Kurtosis', fontsize=font_size)
+    axis.tick_params(axis='x', labelsize=font_size)
+    axis.tick_params(axis='y', labelsize=font_size)
+
+    axis.annotate('Mean=%0.2f' % np.mean(population_sparseness),
+                  xy=(0.95, 0.9),
+                  xycoords='axes fraction',
+                  fontsize=font_size,
+                  horizontalalignment='right',
+                  verticalalignment='top')
+
+    axis.annotate('N=%d' % len(population_sparseness),
+                  xy=(0.95, 0.82),
+                  xycoords='axes fraction',
+                  fontsize=font_size,
+                  horizontalalignment='right',
+                  verticalalignment='top')
+
+    axis.set_title('Population Sparseness Distribution', fontsize=font_size)
 
 
 def plot_population_obj_preferences(it_population, axis=None):
@@ -73,12 +166,12 @@ def plot_selectivity_vs_position_tolerance(it_population, axis=None):
     # Plot the linear regression fit as well.
     m, b = np.polyfit(selectivity, position_tolerance, 1)
 
-    plt.plot(selectivity, m*selectivity + b, ':', color='green')
+    plt.plot(selectivity, m * selectivity + b, ':', color='green')
 
     f_size = 34
     axis.set_ylabel('Position Tolerance (Radians)', fontsize=f_size)
     axis.set_xlabel('Selectivity (Activity Fraction)', fontsize=f_size)
-    axis.set_title('Position Tolerance vs Selectivity',fontsize=f_size+10)
+    axis.set_title('Position Tolerance vs Selectivity', fontsize=f_size + 10)
     axis.tick_params(axis='x', labelsize=f_size)
     axis.tick_params(axis='y', labelsize=f_size)
 
